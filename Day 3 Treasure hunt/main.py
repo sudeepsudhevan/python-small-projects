256122255aa9a47da5e362db5ac20832fc9ff2a3
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2023-08-06 12:12:56 +0530

    Create main.py

---
+++ Day 3 Treasure hunt/main.py
@@ -0,0 +1,47 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+first_choice = input('You\'re on a bridge. What is your choice?, "up", "down", "stay" ...: ').lower()
+if first_choice == "up":
+  print("you have fall into hole")
+elif first_choice == "down":
+  second_choice = input('Now you are infront of river?, "swim", "wait": ').lower()
+  if second_choice == "wait":
+    third_choice = input('which color door are you taking?, "blue", "red", "green": ').lower()
+    if third_choice == "red":
+      print("you found the treasure")
+    elif third_choice == "green":
+      print("burned by fire, game over")
+    elif third_choice == "blue":
+      print("eaten by beast, game over")
+    else:
+      print("game over you are being a fool")
+  else:
+    print("Attacked by crocodile, game over")
+else:
+  print("fall into spider-web, game over")
