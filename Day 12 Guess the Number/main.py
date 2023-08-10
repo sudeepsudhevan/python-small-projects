9fcf0d9099c7a017bcbdfc4747456c720692ba54
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2023-08-10 16:14:38 +0530

    Update main.py

--- Day 12 Guess the Number/main.py
+++ Day 12 Guess the Number/main.py
@@ -1,19 +1,23 @@
 from art import logo
 import random
 
+EASY_TURNS = 5
+HARD_TURNS = 10
+
 print(logo)
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
+
 actual_number = random.randint(1,100)
 
 print(f"Pssst, the correct answer is {actual_number}")
-
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
-
-if difficulty == "easy":
-  attempt = 10
-else:
-  attempt = 5
+def set_difficulty():
+  difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+  
+  if difficulty == "easy":
+    return HARD_TURNS
+  else:
+    return EASY_TURNS
 
 def compare(actual_number,guess_number,attempt):
   """Return the attempt number left"""
@@ -28,13 +32,18 @@ def compare(actual_number,guess_number,attempt):
     return attempt - 1
 
 
-while not attempt == 0:
-  print(f"You have {attempt} attempts remaining to guess the number.")
-  guess_number = int(input("Make a guess: "))
-  attempt = compare(actual_number,guess_number,attempt)
-  if attempt != 0:
-    print("Guess Again")
 
+def game():
+  attempt = set_difficulty()
+  while not attempt == 0:
+    print(f"You have {attempt} attempts remaining to guess the number.")
+    guess_number = int(input("Make a guess: "))
+    attempt = compare(actual_number,guess_number,attempt)
+    if attempt != 0:
+      print("Guess Again")
+  
+  
+  if attempt == 0 and guess_number != actual_number:
+    print(f"You've run out of guesses, you lose.\nThe actual number is {actual_number}")
 
-if attempt == 0 and guess_number != actual_number:
-  print("You've run out of guesses, you lose.")
+game()
