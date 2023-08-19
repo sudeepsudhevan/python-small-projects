aeebc378ab843501652a612b5ce8e65001d63383
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-08-19 16:55:16 +0530

    snake game day 20

--- Day 20 and 21 Snake Game/main.py
+++ Day 20 and 21 Snake Game/main.py
@@ -1,1 +1,29 @@
-i
+from turtle import Screen
+import time
+from snake import Snake
+
+screen = Screen()
+
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+screen.tracer(0)
+
+snake = Snake()
+screen.listen()
+
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(1)
+
+    snake.move()
+
+screen.exitonclick()
