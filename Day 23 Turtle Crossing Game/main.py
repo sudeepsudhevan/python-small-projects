ea641dd70b6595e1b22ae6e80d00ffe7a7e5601a
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-08-22 13:13:13 +0530

    crossing game

---
+++ Day 23 Turtle Crossing Game/main.py
@@ -0,0 +1,37 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(fun=player.move_up, key="space")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    car_manager.create_car()
+    car_manager.move_car()
+
+    # Detect turtle collide with car
+    for car in car_manager.all_car:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    # Detect turtle reached the other side
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.level_increase()
+
+screen.exitonclick()
