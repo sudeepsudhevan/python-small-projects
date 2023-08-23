baded4d14fcb21d724b1c707aea3bcfead5a9973
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2023-08-23 18:15:43 +0530

    Update main.py

--- Day 24 Mail Merge Project/My_method/main.py
+++ Day 24 Mail Merge Project/My_method/main.py
@@ -1,17 +1,13 @@
 with open("input/Letters/starting_letter.txt") as letter:
-    invite_letter = letter.readlines()
-    later = ''.join([str(elem) for elem in invite_letter[1:]])
+    invite_letter = letter.read()
 
-# print(invite_letter)
-# print(invite_letter[1:])
 
 with open("input/Names/invited_names.txt") as names:
     name_list = names.readlines()
-print(name_list)
+
 for name in name_list:
     stripped_name = name.strip()
-    changed_sentence = invite_letter[0].replace("[name]", f"{stripped_name}")
-    # print(changed_sentence)
+    changed_sentence = invite_letter.replace("[name]", f"{stripped_name}")
 
     with open(f"output/ReadyToSend/letter_for_{stripped_name}", mode="w") as file:
-        file.write(f"{changed_sentence}{later}")
+        file.write(f"{changed_sentence}")
